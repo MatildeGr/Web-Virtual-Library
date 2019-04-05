6e9d5d8c3a1f65530715d209150fe9a9f5c4b850
Matilde Gravano <matilde.gravano@gmail.Com>
2019-04-05 09:18:54 +0200

    Signup mdp et mdpconfirm : done

--- controller/ControllerMain.php
+++ controller/ControllerMain.php
@@ -39,11 +39,10 @@ class ControllerMain extends ControllerBis {
             $password_confirm = trim($_POST['password_confirm']);
             $email = trim($_POST['email']);
             $birthdate = trim($_POST['birthdate']);
-            if ($password !== $password_confirm) {
-                $errors = "You have to enter twice the same password.";
-            }
             $errors = (new User($fullname, $username, $password, $email, null, $birthdate, null))->validate_user();
-
+            if ($password !== $password_confirm) {
+                $errors[] = "You have to enter twice the same password.";
+            }
             if (count($errors) == 0) {
                 User::add_user($username, $password, $fullname, $email, $birthdate);
                 $this->log_user(User::get_user_by_username($username));
@@ -53,4 +52,5 @@ class ControllerMain extends ControllerBis {
             "password_confirm" => $confirm_password, "fullname" => $fullname, "email" => $email,
             "birthdate" => $birthdate, "errors" => $errors));
     }
+
 }
